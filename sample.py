f1ca558973ac38f90486761c89403183f275d884
markocrnic <markons996@gmail.com>
2019-09-20 11:37:41 +0200

    Created first flask helloworld app.

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
-print ('Hello World!')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/helloworld/')
+def helloworld():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    app.run()
